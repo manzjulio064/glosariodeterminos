7da0317d3dc368b0f14a86961a50fb6750ce3d54
Hector Javier Moreno <benjumeacarlos981@gmail.com>
2024-04-24 23:02:23 -0500

    deployando gh-pages

---
+++ PythonCondicionalIf.py
@@ -0,0 +1,17 @@
+
+# edad = int(input("ingrese su edad "))
+
+# if edad > 17:
+#     print("Estas viejo")
+# else:
+#     print("Eres joven")
+
+
+numero = int(input("Ingrese un numero: "))
+
+if numero > 5:
+    print("El numero es mayor a 5")
+elif numero == 5:
+    print("El numero es 5 ")
+else:
+    print("El numero es menor a 5 ")
