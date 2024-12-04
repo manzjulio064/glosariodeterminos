25f477198c341d168c48c043f946fe1f889b6fd1
Hector Javier Moreno <benjumeacarlos981@gmail.com>
2024-12-04 12:32:04 -0500

    deployando gh-pages

--- EspBackEndIIC7AKeycloakSpringBooResumen.js
+++ EspBackEndIIC7AKeycloakSpringBooResumen.js
@@ -1,24 +1,42 @@
-// URL del archivo que deseas descargar
-const fileUrl = './springboot-keycloak-luisina.zip';
+// URL de los archivos que deseas descargar
+const fileUrl1 = './springboot-keycloak-luisina.zip';
+const fileUrl2 = './Keycloak+SpringBoot_LuisinaCollection.postman_collection.json';
 
 // Función para manejar la descarga
 function setupDownloadButton() {
-  // Obtener el botón por su ID
-  const downloadButton = document.getElementById('downloadBtn');
+  // Obtener los botones por sus IDs
+  const downloadButton1 = document.getElementById('downloadBtn1');
+  const downloadButton2 = document.getElementById('downloadBtn2');
 
-  if (downloadButton) {
-    // Agregar un evento de clic al botón
-    downloadButton.addEventListener('click', function () {
-      // Crear un elemento <a> dinámico
+  console.log('downloadButton1:', downloadButton1);
+  console.log('downloadButton2:', downloadButton2);
+
+  // Configurar evento para el primer botón
+  if (downloadButton1) {
+    downloadButton1.addEventListener('click', function () {
       const a = document.createElement('a');
-      a.href = fileUrl; // Establecer la URL del archivo
-      a.download = 'springboot-keycloak-luisina.zip'; // Establecer el nombre del archivo
-      document.body.appendChild(a); // Agregar el elemento al DOM
-      a.click(); // Simular un clic en el enlace
-      document.body.removeChild(a); // Eliminar el elemento después del clic
+      a.href = fileUrl1; // Establecer la URL del archivo
+      a.download = 'springboot-keycloak-luisina.zip'; // Nombre del archivo
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
     });
   } else {
-    console.error('El botón con ID "downloadBtn" no se encontró en el DOM.');
+    console.error('El botón con ID "downloadBtn1" no se encontró en el DOM.');
+  }
+
+  // Configurar evento para el segundo botón
+  if (downloadButton2) {
+    downloadButton2.addEventListener('click', function () {
+      const a = document.createElement('a');
+      a.href = fileUrl2; // Establecer la URL del archivo
+      a.download = 'Keycloak+SpringBoot_LuisinaCollection.postman_collection.json'; // Nombre del archivo
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    });
+  } else {
+    console.error('El botón con ID "downloadBtn2" no se encontró en el DOM.');
   }
 }
 
